951cb1f8d10cea39baa19bf16ed479cfe6888386
rania <raniakittan@hotmail.com>
2023-07-08 15:34:27 +0300

    removed duplicated files

--- src/cmps/UserMessage (2).jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const UserMessage = ({ message }) => {
-  return <div className='user-message'>{message}</div>
-}
-
-export default UserMessage
--- src/cmps/colors (2).jsx
+++
@@ -1,9 +0,0 @@
-export const predefinedColors = [
-  '#0077c2',
-  '#0099e6',
-  '#00bfff',
-  '#33ccff',
-  '#66d9ff',
-  '#99e6ff',
-  '#cceeff'
-]
